01ebc4cf9db3b2582df1cbaad3504056c1a1f3d7
dgko94 <llkk1346@naver.com>
2021-04-29 23:43:51 +0900

    back-end

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,11 @@
+package hello.servlet.basic;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+
+}
